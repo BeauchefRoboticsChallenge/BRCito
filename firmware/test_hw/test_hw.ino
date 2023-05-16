d7035ab8c370e1dfaa5d84d75ef196266a5a87a8
Gonzalo Olave <growolff@gmail.com>
2023-05-16 13:28:39 -0400

    update files

--- firmware/test_hw/test_hw.ino
+++ firmware/test_hw/test_hw.ino
@@ -34,8 +34,8 @@ void setup() {
   // boton
   pinMode(BUTTON_PIN, INPUT_PULLUP);
   // sensores
-  qtr.setTypeRC();
-  qtr.setSensorPins((const uint8_t[]){2,3,A0,A1,A2,A3,A4,A5}, 8);
+  qtr.setTypeAnalog();
+  qtr.setSensorPins((const uint8_t[]){A0,A1,A2,A3,A4,A5,A6,A7}, 8);
 
   // motores
   pinMode(ML_IN1,OUTPUT);
@@ -48,8 +48,8 @@ void setup() {
 }
 
 void loop() {
-  //testButton();
-  testLed();
+  testButton();
+  //testLed();
   //testSensors();
   //testMotors();
 
